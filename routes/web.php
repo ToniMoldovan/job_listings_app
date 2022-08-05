ff057d67c2e42c2e811644e03a2ffa5398c1d53c
Toni <antonio.moldovan82@yahoo.com>
2022-08-05 19:57:45 +0300

    updated route

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,8 @@ Route::get('/', function () {
 });
 
 // Return single listing by ID
-Route::get('/listings/{id}', function($id){
+Route::get('/listings/{listing}', function(Listing $listing){
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });
