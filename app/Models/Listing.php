1cdfd03bbdf188f042ab22c9e6af60e7e11ae0c3
Toni <antonio.moldovan82@yahoo.com>
2022-08-05 13:47:37 +0300

    deleted old manual model & created new one using artisan

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,30 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-                [
-                    'id' => 1,
-                    'title' => 'Listing One',
-                    'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis dolore saepe provident earum vel porro dicta reiciendis! Odit quos doloribus explicabo dolorem, deserunt delectus ut beatae odio itaque repellendus facere?'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Listing Two',
-                    'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis dolore saepe provident earum vel porro dicta reiciendis! Odit quos doloribus explicabo dolorem, deserunt delectus ut beatae odio itaque repellendus facere?'
-                ]
-        ];
-    }
-
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id)
-                return $listing;
-        }
-    }
+    use HasFactory;
 }
